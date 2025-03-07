a2ed39d421a98b519194e202ae91b85e95d9f080
mihir-r-kondapalli <mihir.kondapalli@gmail.com>
2025-03-07 11:57:20 -0800

    vercel fix

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ export default function Page() {
     
     setIsLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/generate-image/", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/generate-image/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(params),
